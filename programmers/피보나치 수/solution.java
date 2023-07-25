231239150c58d2cae1cb26dc45fb3d72ebfb7671
GeonSang Lee <116617354+geonsangg@users.noreply.github.com>
2023-07-25 13:04:10 +0900

    uploaded at 2023. 7. 25. 오후 1:04:20

---
+++ programmers/피보나치 수/solution.java
@@ -0,0 +1,15 @@
+// [문제 링크]: https://school.programmers.co.kr/learn/courses/30/lessons/12945
+
+class Solution {
+    public int solution(int n) {
+        int answer = 0;
+        int[] arr = new int[n+1];
+        arr[0] = 0;
+        arr[1] = 1;
+        for(int i=2; i<=n; i++) {
+            arr[i] = ((arr[i-1] % 1234567) + (arr[i-2] % 1234567)) % 1234567;
+        }
+        answer = arr[n];
+        return answer;
+    }
+}
